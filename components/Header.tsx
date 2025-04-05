8cda612dd7a1d95121863be658c641f1e0a673ad
Karuna Solanki <karuna.solanki@outlook.com>
2025-04-05 21:20:59 +0200

    Add about page content and link changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <header className={headerClass}>
-      <Link href="/" aria-label={siteMetadata.headerTitle}>
+      {/* <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
             <Logo />
@@ -27,20 +27,18 @@ const Header = () => {
             siteMetadata.headerTitle
           )}
         </div>
-      </Link>
-      <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
+      </Link> */}
+      <div className="flex w-full items-center justify-end space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
-          {headerNavLinks
-            .filter((link) => link.href !== '/')
-            .map((link) => (
-              <Link
-                key={link.title}
-                href={link.href}
-                className="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100"
-              >
-                {link.title}
-              </Link>
-            ))}
+          {headerNavLinks.map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100"
+            >
+              {link.title}
+            </Link>
+          ))}
         </div>
         <SearchButton />
         <ThemeSwitch />
